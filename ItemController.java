b5b33ed6e5e2497ecdd4f7a69f00f11a87b4eee4
shahiraja <mdshahiraja195@gmail.com>
2026-03-27 16:09:16 +0600

    Add files via upload

---
+++ ItemController.java
@@ -0,0 +1,54 @@
+package com.example.cafeteriamangement;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemController implements Initializable {
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label itemName;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Spinner<Integer> itemQuantity;
+
+    private Item item;
+
+    private Controller mainController;
+
+    public void setMainController(Controller controller){
+        this.mainController = controller;
+    }
+
+    public void setData(Item item){
+        this.item = item;
+        itemName.setText(item.getName());
+        priceLabel.setText(item.getPrice() + Main.Currency);
+        Image image = new Image(getClass().getResourceAsStream(item.getImgsrc()));
+        img.setImage(image);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<Integer> valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10);
+        valueFactory.setValue(1);
+        itemQuantity.setValueFactory(valueFactory);
+    }
+    public void handleAdd(){
+        int quantity = itemQuantity.getValue();
+        mainController.addItemToTable(item, quantity);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.example.cafeteriamangement;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static final String Currency = " Tk";
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
